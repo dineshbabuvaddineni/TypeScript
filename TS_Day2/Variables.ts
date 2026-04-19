f05a55332a67d9d9f8fa4a60eba6c14302b10e62
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-04-19 13:42:14 +0530

    Typescript Day1

---
+++ TS_Day2/Variables.ts
@@ -0,0 +1,9 @@
+//Single line comment   ctr+/ --> Windows   cmd + /
+console.log("Welocme to Typescript")
+
+//Muliple Line comment  Shift+Alt+A --> Windows   Shift+Option+A
+console.log("Welocme to Typescript")
+console.log("Welocme to Typescript")
+console.log("Welocme to Typescript")
+console.log("Welocme to Typescript")
+console.log("Welocme to Typescript")
