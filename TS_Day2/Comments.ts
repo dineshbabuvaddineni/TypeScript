05a1c54a6f59480f43f6938e53b755871fa12c04
dineshbabuvaddineni <dineshbabuvaddineni@gmail.com>
2026-04-20 20:30:49 +0530

    comments and variables

--- TS_Day2/Comments.ts
+++ TS_Day2/Comments.ts
@@ -1,7 +1,7 @@
-//Single line comment   ctr+/ --> Windows   cmd + /
+//Single line comment   ctr+/ --> Windows   cmd + / --> macos
 console.log("Welocme to Typescript")
 
-//Muliple Line comment  Shift+Alt+A --> Windows   Shift+Option+A
+//Muliple Line comment  Shift+Alt+A --> Windows   Shift+Option+A --> macos
 console.log("Welocme to Typescript")
 console.log("Welocme to Typescript")
 console.log("Welocme to Typescript")
